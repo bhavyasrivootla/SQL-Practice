af567a48d41c19303ee43e015fe5332bfe43f079
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-06-27 22:13:38 +0530

    Create 586. Customer Placing the Largest Number of Orders.sql

---
+++ Basic Select/586. Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,26 @@
+/*
+Example 1:
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+*/
+
+select customer_number
+from Orders
+group by customer_number
+order by count(*) desc
+limit 1;
